775093d69a2f7f867ec327e4cd261121e1f1907e
ag.thanhz <ag.thanhz@gmail.com>
2022-02-13 10:01:57 +0700

    update code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
 import HomeComponent from "../components/Home/Home.template";
 
 const Index = ({ homeData }: any) => {
-  return <HomeComponent {...homeData} />;
+  return <HomeComponent />;
 };
 
 export default Index;
-
-export async function getStaticProps() {
-  const data = await fetch("http://s2api.test/api/v1/categories");
-  const categories = await data.json();
-  console.log(categories[0].slug);
-  return {
-    props: { homeData: { categories } },
-  };
-}
